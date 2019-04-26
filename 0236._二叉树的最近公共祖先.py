31537f48db52e1b21d68657170a34f9b3ce281d5
Crow zhao <530902730@qq.com>
2019-04-26 23:23:51 +0800

    2019.4.26

---
+++ 0236._二叉树的最近公共祖先.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# 问题转化为 两个单向链表的第一个公共结点
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+
+        if root == None or root == p or root == q:
+            return root
+
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        # p, q 不存在祖先关系
+        if left and right:
+            return root
+        return left or right
+
